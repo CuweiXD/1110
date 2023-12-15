0a3f27bb9cc984b4b324590ad14c2f7cc3006be1
CuweiXD <126766439+CuweiXD@users.noreply.github.com>
2023-12-15 10:57:20 +0800

    Create 2023-12-15.py
    1215-Class Practice

---
+++ 2023-12-15.py
@@ -0,0 +1,30 @@
+'''
+class Student:
+    def __init__(self,ID,Name,Department):
+
+
+
+
+     s1=Student('Steve','4A23','Information Engineering')
+     s1.save_to_database
+     s2=Student('Meg','2C33','Information management')    
+     '''
+
+class Student:
+    def __init__(self,ID,Name,Department):
+        self._ID=ID
+        self._Name=Name
+        self._Department=Department
+
+class Teacher:
+    pass
+
+s1 = Student('4B22','Steve','Information Engineering')
+print("Student s1's Name=",s1._Name)
+print("Student s1's ID=",s1._ID)
+print("Student s1's Department=",s1._Department)
+
+s2=Student('4A14','Alex','Information management')
+print("Student s2's Name=",s2._Name)
+print("Student s2's ID=",s2._ID)
+print("Student s2's Department=",s2._Department)
